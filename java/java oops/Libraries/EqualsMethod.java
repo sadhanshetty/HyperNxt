b9c1b4fadcc13cfa708e2015ea4535085d5a00a3
sadhan shetty <136951498+sadhanshetty@users.noreply.github.com>
2024-08-03 10:21:47 +0530

    Add files via upload

---
+++ java/java oops/Libraries/EqualsMethod.java
@@ -0,0 +1,12 @@
+public class EqualsMethod{
+    public static void main(String[] args) {
+        EqualsMethod e1 = new EqualsMethod();
+        EqualsMethod e2 = new EqualsMethod();
+        System.out.println(e1.equals(e2));
+
+    }
+
+    public boolean equals(Object s2){
+        return true;
+    }
+}
