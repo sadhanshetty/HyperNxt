d6f236837730043b07137290db3fdfbe964f68ba
sadhan shetty <136951498+sadhanshetty@users.noreply.github.com>
2024-08-03 10:20:25 +0530

    Add files via upload

---
+++ java/java oops/Libraries/HashCodeMethod.java
@@ -0,0 +1,11 @@
+public class HashCodeMethod {
+    public static void main(String[] args) {
+        
+        HashCodeMethod h = new HashCodeMethod();
+        System.out.println(h.hashCode());
+
+    }
+    public int hashCode(){
+        return 123;
+    }
+}
